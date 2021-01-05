61bbe938ab3bca8bff7b850520833d1c71687648
Lobo <matheuscardoso1@poli.ufrj.br>
2021-01-05 00:26:06 -0300

    Debugged some little issues

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import styles from './Footer.module.css'
-import nytLogo from '../../images/nyt_logo.png'
 
 class Footer extends Component {
     state = {  }
     render() { 
         return (
             <div className={styles.footer}>
-                <img src='https://developer.nytimes.com/files/poweredby_nytimes_200b.png?v=1583354208360'></img>
+                <img alt='New York Times Attribution' src='https://developer.nytimes.com/files/poweredby_nytimes_200b.png?v=1583354208360'></img>
                 <p>&copy; Lupus Magnus, 2020. </p>
                 
             </div>
