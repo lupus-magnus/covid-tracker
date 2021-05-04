26da27719f4a6fd5c3714670a8b9e351e1f433bf
Lobo <matheuscardoso1@poli.ufrj.br>
2021-05-04 12:10:56 -0300

    corrigi bug da thumb inexistente, que causava o site a sair do ar

--- src/api/index.js
+++ src/api/index.js
@@ -1,81 +1,64 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'https://covid19.mathdro.id/api'
+const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async (country) => {
-    let changeableUrl = url;
-    if (country){
-        changeableUrl = `${url}/countries/${country}`
-    }
-    try{
-        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(changeableUrl)
-        const modifiedData = {confirmed, recovered, deaths, lastUpdate}
-        
-        return (modifiedData)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  let changeableUrl = url;
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableUrl);
+    const modifiedData = { confirmed, recovered, deaths, lastUpdate };
+
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const fetchDailyData = async () => {
-    try{
-        const { data } = await axios.get(`${url}/daily`)
-        const modifiedData = data.map((dailyData) => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            date: dailyData.reportDate,
-        }))
-        return modifiedData
-    } catch(error){
-        console.log(error)
-    }
-}
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const fetchCountries = async () => {
-    try {
-        const {data: { countries }} = await axios.get(`${url}/countries`)
-        return countries.map((country) => country.name)
-    } catch(error){
-        console.log(error)
-    }
-}
-
-//Seção de api das notícias.
-let keyGmail = '3234e3e9c25e458dae03ccec0b3d134a'
-let keyPoli = '9431beab82af4055b6fd0c27dd9b3d74'
-let keyList = [keyGmail,keyPoli]
-let urlNews = 'https://newsapi.org/v2/top-headlines?country=br&q=vacina&from=2020-12-18&sortBy=popularity&apiKey=' + keyList[Math.floor(Math.random()*keyList.length)] +'&language=pt';
-let proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-
-
-export const getNews = async () => {
-    const request = new Request(proxyUrl+urlNews) //nova!
-    const response = await fetch(request, {type: 'no-cors'}) //nova
-    console.log('A request was just made to the News Api.')
-    console.log('response:', response)
-    const data = await response.json()
-    console.log('response: ', data)
-    const modifiedData = data.articles.slice(0,4)
-    console.log('data:', data)
-    console.log ('modifiedData: ', modifiedData)
-    return (modifiedData)
-}
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //Seção de API das notícias usando o New York Times.
 
-let nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=corona+virus&sort=newest&api-key=YYixc9rfwwSnyP5EQMbQJAAK6C8DpAt2'
+let nytUrl =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=corona+virus&sort=newest&api-key=YYixc9rfwwSnyP5EQMbQJAAK6C8DpAt2";
 
 export const getNYTNews = async () => {
-    const response = await fetch(nytUrl)
-    console.log('----------------------------')
-    console.log('A request was just made to the New York Times API.')
-    const data = await response.json()
-    console.log('NewYorkTimes response: ', data)
-    const news = await data.response.docs
-    console.log('NewYorkTimes news: ',news)
-    const finalData = news.slice(0,4)
-    console.log('NYT final data:', finalData)
-    console.log('----------------------------')
-    return(finalData)
-    
-}
+  const response = await fetch(nytUrl);
+  console.log("----------------------------");
+  console.log("A request was just made to the New York Times API.");
+  const data = await response.json();
+  console.log("NewYorkTimes response: ", data);
+  const news = await data.response.docs;
+  console.log("NewYorkTimes news: ", news);
+  const finalData = news.slice(0, 4);
+  console.log("NYT final data:", finalData);
+  console.log("----------------------------");
+  return finalData;
+};
--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -1,74 +1,81 @@
-import React, { Component } from 'react';
-import { getNYTNews } from '../../api/index'
-import styles from '../Slider/slider.module.css'
-import genericImage from '../../../src/images/newspaper.jpg'
+import React, { Component } from "react";
+import { getNYTNews } from "../../api/index";
+import styles from "../Slider/slider.module.css";
+import genericImage from "../../../src/images/newspaper.jpg";
 
 export class Carousel extends Component {
-    constructor(props) {
-        console.log('Constructor log.')
-        super(props)
-    
-        this.state = {
-            news: {},
-            active: 0,
-        }
+  constructor(props) {
+    console.log("Constructor log.");
+    super(props);
+
+    this.state = {
+      news: {},
+      active: 0,
+    };
+  }
+
+  updateActive() {
+    this.setState({ active: this.state.active + 1 });
+  }
+
+  componentDidMount() {
+    console.log("Component did mount block!");
+    const fetchAPI = async () => {
+      this.setState({ news: await getNYTNews() });
+      //console.log('We successfully made a request. -Logged from componentDidMount')
+    };
+    fetchAPI();
+    //console.log('End of componentDidMount!')
+    //console.log('----------------')
+
+    window.setInterval(() => this.updateActive(), 7500);
+  }
+
+  render() {
+    let { news, active } = this.state;
+    let currentNew = news[active % news.length];
+    //let main = currentNew.headline.main
+    //let print_hline = currentNew.headline.print_head
+
+    if (news.length > 2) {
+      return (
+        <div className={styles.slidecontainer}>
+          <a rel="noreferrer" target="_blank" href={currentNew.web_url}>
+            <div id="slider">
+              <img
+                className={styles.slidephoto}
+                alt="Imagem ilustrativa da notícia."
+                src={
+                  currentNew.multimedia[9]
+                    ? "https://www.nytimes.com/" + currentNew.multimedia[9].url
+                    : genericImage
+                }
+                name="slide"
+              />
+              <p className={styles.newsText}>{currentNew.headline.main}</p>
+            </div>
+          </a>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles.slidecontainer}>
+          <div id="slider">
+            <img
+              className={styles.slidephoto}
+              alt="foto da noticia"
+              src="https://www.nbn.org.il/wp-content/uploads/2014/11/scientificresearch_29824805_cropped.jpg"
+              name="slide"
+              rel="noreferrer"
+            />
+            <p className={styles.newsText}>
+              Não há novas notícias nesse momento.
+            </p>
+          </div>
+        </div>
+      );
     }
-
-    updateActive() {
-        this.setState({active: this.state.active + 1})
-    }
-
-    componentDidMount() {
-        console.log('Component did mount block!')
-        const fetchAPI = async () => {
-            this.setState({news: await getNYTNews()})
-            //console.log('We successfully made a request. -Logged from componentDidMount')
-        }
-        fetchAPI()
-        //console.log('End of componentDidMount!')
-        //console.log('----------------')
-
-        window.setInterval(() => this.updateActive(), 7500)
-    }
-
-    render() {
-        let {news, active} = this.state;
-        let currentNew = news[active%news.length]
-        //let main = currentNew.headline.main
-        //let print_hline = currentNew.headline.print_head
-
-        if(news.length > 2){
-            return (
-                <div className={styles.slidecontainer}>
-                    <a rel="noreferrer" target="_blank" href={currentNew.web_url}>
-                        <div id='slider'>
-                            <img className={styles.slidephoto} alt='Imagem ilustrativa da notícia.'
-                                src={('https://www.nytimes.com/' + currentNew.multimedia[9].url) || genericImage}
-                                name='slide' 
-                            />
-                            <p className={styles.newsText}>{currentNew.headline.main}</p>
-                            
-                        
-                        </div>
-                    </a>
-                </div>
-            );
-        } else {
-            return (
-                <div className={styles.slidecontainer}>
-                    <div id='slider'>
-                        <img className={styles.slidephoto} alt='foto da noticia'
-                            src='https://www.nbn.org.il/wp-content/uploads/2014/11/scientificresearch_29824805_cropped.jpg'
-                            name='slide'
-                            rel="noreferrer" 
-                        />
-                        <p className={styles.newsText}>Não há novas notícias nesse momento.</p>
-                    </div>
-                </div>
-            )
-        }
-    }
-
+  }
 }
- 
+
 export default Carousel;
